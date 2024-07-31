9f94bf7462ab022681d2ae3b7677218d849eb5c3
Thirupathilaxmi18 <s.lucky1805@gmail.com>
2024-07-31 10:52:38 +0530

    {hi}

---
+++ calculator.py
@@ -0,0 +1,12 @@
+'''2.Create a Python module named calculator.py that contains functions for addition,
+subtraction, multiplication, and division. Write a program to import this module and
+perform arithmetic operations on two numbers entered by the user.'''
+def addition(a,b):
+    return a+b
+def subtraction(a,b):
+    return a-b
+def multiplication(a,b):
+    return a*b
+def division(a,b):
+    return a/b
+
---
+++ class basics.py
@@ -0,0 +1,6 @@
+def keyars(test="1",test2="2"):
+    print(test,test2)
+keyars()
+def my_fun(x):
+    return 5*x
+print(my_fun(5))
